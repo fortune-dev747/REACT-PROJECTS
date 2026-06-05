e79a4322b10ea95c97a42480cb662b9d244f0729
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-05 04:25:34 +0100

    updated

---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev';
+import LoadingSpinner from '../assets/loading-spinner.gif';
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  function saveInputText(event) {
+   setInputText(event.target.value);
+  }
+
+    // added async / await features to handle the asynchronous response from the chatbot
+    async function sendMessage() {
+        if (isLoading || inputText === '') {
+        return;
+        }
+        // Set isLoading to true at the start, and set it to
+        // false after everything is done.
+        setIsLoading(true);
+        // We can put this at the top of the function or
+        // after the first setChatMessages(). Both work.
+        setInputText(''); // Clear the input field immediately after sending the message
+
+        const newChatMessages = [
+        ...chatMessages,
+        {
+            message: inputText,
+            sender: 'user',
+            id: crypto.randomUUID()
+        }
+        ];
+
+        setChatMessages([
+        ...newChatMessages,
+        // This creates a temporary Loading... message.
+        // Because we don't save this message in newChatMessages,
+        // it will be remove later, when we add the response.
+        {
+            message: <img className="loading-spinner" src={LoadingSpinner} />,
+            sender: 'robot',
+            id: crypto.randomUUID()
+        }
+        ]);
+
+
+        const response = await Chatbot.getResponseAsync(inputText);
+        setChatMessages([
+        ...newChatMessages,
+        {
+            message: response,
+            sender: 'robot',
+            id: crypto.randomUUID()
+        }
+        ]);
+
+        // Set isLoading to false after everything is done.
+        setIsLoading(false);
+
+    }
+
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+        sendMessage();
+        } else if (event.key === 'Escape') {
+        setInputText('');
+        }
+    }
+
+    return (
+        <div className="chat-input-container">
+        <input 
+            placeholder= "Send a message to Chatbot"
+            size= "30" 
+            onChange={saveInputText}
+            value={inputText}  //=> controlled input
+            //  onKeyDown => new events prop for both enter and escape key
+            onKeyDown={handleKeyDown}
+            className="chat-input"
+        />
+        <button
+            onClick={sendMessage}
+            className="send-button"
+        >Send</button>
+        </div>
+    );
+    }
---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react'
+import { ChatMessage } from './ChatMessage';
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElement = chatMessagesRef.current;
+    if (containerElement) {
+      containerElement.scrollTop = containerElement.scrollHeight;
+    }
+  }, [chatMessages]);
+  return (
+    <div className="chat-messages-container" 
+      ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage 
+            message={chatMessage.message}
+            sender={chatMessage.sender} 
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
