e78fd45a65661977002cecb213c8f31834c7238a
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-06 20:53:43 +0100

    removed the logo and added text

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -10,7 +10,7 @@ export function CheckoutPage() {
             <div className="header-content">
                 <div className="checkout-header-left-section">
                 <a href="/">
-                    {/* <img className="logo" src="images/logo.png" /> */}
+                    <p>Back</p>
                     <img className="mobile-logo" src="images/mobile-logo.png" />
                 </a>
                 </div>
