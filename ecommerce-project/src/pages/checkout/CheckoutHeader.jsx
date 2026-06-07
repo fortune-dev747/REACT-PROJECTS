70f91cce2918dcbc4d92a4e5b20c1824ba483b37
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-07 02:35:23 +0100

    replaced all the a tag with Link

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -5,15 +5,15 @@ export function CheckoutHeader() {
         <div className="checkout-header">
             <div className="header-content">
                 <div className="checkout-header-left-section">
-                <a href="/">
+                <Link href="/">
                     <p>Back</p>
                     <img className="mobile-logo" src="images/mobile-logo.png" />
-                </a>
+                </Link>
                 </div>
 
                 <div className="checkout-header-middle-section">
-                Checkout (<a className="return-to-home-link"
-                    href="/">3 items</a>)
+                Checkout (<Link className="return-to-home-link"
+                    href="/">3 items</Link>)
                 </div>
 
                 <div className="checkout-header-right-section">
