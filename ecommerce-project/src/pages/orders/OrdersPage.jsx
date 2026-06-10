1f2dc0f72c3c376bb9509898704d8f2501c7ea06
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-10 19:40:12 +0100

    moved orders grid into its own components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router'
 import { Header } from '../../components/Header'
 import './OrdersPage.css';
 import { formatMoney } from '../../utils/money';
+import { OrdersGrid } from './OrdersGrid';
 
 export function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
@@ -51,41 +52,7 @@ export function OrdersPage({ cart }) {
                                     </div>
                                 </div>
 
-                                <div className="order-details-grid">
-                                    {order.products.map((orderProduct) => {
-                                        return (
-                                            <Fragment key={orderProduct.product.id}>
-                                                <div className="product-image-container">
-                                                    <img src={orderProduct.product.image} />
-                                                </div>
-
-                                                <div className="product-details">
-                                                    <div className="product-name">
-                                                       {orderProduct.product.name}
-                                                    </div>
-                                                    <div className="product-delivery-date">
-                                                        Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
-                                                    </div>
-                                                    <div className="product-quantity">
-                                                        Quantity: {orderProduct.quantity}
-                                                    </div>
-                                                    <button className="buy-again-button button-primary">
-                                                        <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                                                        <span className="buy-again-message">Add to Cart</span>
-                                                    </button>
-                                                </div>
-
-                                                <div className="product-actions">
-                                                    <Link to="/tracking">
-                                                        <button className="track-package-button button-secondary">
-                                                            Track package
-                                                        </button>
-                                                    </Link>
-                                                </div>
-                                            </Fragment>
-                                        )
-                                    })}
-                                </div>
+                                <OrdersGrid order={order} />
                             </div>
                         )
                     })}
