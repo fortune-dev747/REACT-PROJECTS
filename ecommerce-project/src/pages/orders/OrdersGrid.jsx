337dd1acca3858908c10f8347f73aa6e03ed9bdd
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-10 19:52:38 +0100

    moved order header and order details grid into its own component

--- ecommerce-project/src/pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Link } from "react-router";
 import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
+import { OrderHeader } from "./OrderHeader";
 
 export function OrdersGrid({ orders }) {
     return (
@@ -10,23 +10,7 @@ export function OrdersGrid({ orders }) {
                 return (
                     <div key={order.id} className="order-container">
 
-                        <div className="order-header">
-                            <div className="order-header-left-section">
-                                <div className="order-date">
-                                    <div className="order-header-label">Order Placed:</div>
-                                    <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
-                                </div>
-                                <div className="order-total">
-                                    <div className="order-header-label">Total:</div>
-                                    <div>{formatMoney(order.totalCostCents)}</div>
-                                </div>
-                            </div>
-
-                            <div className="order-header-right-section">
-                                <div className="order-header-label">Order ID:</div>
-                                <div>{order.id}</div>
-                            </div>
-                        </div>
+                        <OrderHeader order={order} />
 
                         <div className="order-details-grid">
                             {order.products.map((orderProduct) => {
